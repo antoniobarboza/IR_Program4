f155462ddc6f6fbc12fbcbe7b7c674aa68af256d
Bobby Chisholm <rwc1007@wildcats.unh.edu>
2019-11-03 15:24:08 -0500

    bobby-working on the custom similarities

--- src/main/java/lucene/CustomSimilarity.java
+++ src/main/java/lucene/CustomSimilarity.java
@@ -11,24 +11,72 @@ import org.apache.lucene.search.similarities.SimilarityBase;
 public class CustomSimilarity {
 	/**
 	 * Returns a custom scoring function
-	 * @return a custom similarity
+	 * 
+	 * @param simName name of which similarity that we will be using, the initials
+	 * @return a custom similarity based on selection, uds is default
 	 */
-	public static Similarity getSimilarity() {
-        SimilarityBase mySimilarity = new SimilarityBase() {
+	public static Similarity getSimilarity(String simName) {
+        simName = simName.toLowerCase();
+		switch(simName) {
+			case "ul":
+				return getUL();
+			case "ujm":
+				return getUJM();
+			default:
+				return getUDS();
+		
+		}
+    }
+	
+	//Comments will be added and these names will be changed to be more accurate
+	private static Similarity getUL() {
+		SimilarityBase ul = new SimilarityBase() {
         	@Override
             protected float score(BasicStats basicStats, float v, float v1) {
+        		
                 return v;
             }
 
             @Override
             public String toString() {
-                return "CustomSimilarity";
+                return "Unigram language model with Laplace smoothing";
             }
         };
-
-        return mySimilarity;
-    }
+        return ul;
+	}
 	
+	private static Similarity getUJM() {
+		SimilarityBase ujm = new SimilarityBase() {
+        	@Override
+            protected float score(BasicStats basicStats, float v, float v1) {
+        		
+                return v;
+            }
+
+            @Override
+            public String toString() {
+                return "Unigram language model with Jelinek-Mercer smoothing";
+            }
+        };
+        return ujm;
+	}
+
+	private static Similarity getUDS() {
+		SimilarityBase uds = new SimilarityBase() {
+        	@Override
+            protected float score(BasicStats basicStats, float v, float v1) {
+        		
+                return v;
+            }
+
+            @Override
+            public String toString() {
+                return "Unigram language model with Dirichlet smoothing";
+            }
+        };
+        return uds;
+	}
+
 	/**
 	 * Returns the name of the ranking function
 	 */
