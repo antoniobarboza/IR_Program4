5590f2094dd9a9fe1f8298ea53a04a4c4380bfc7
Bobby Chisholm <rwc1007@wildcats.unh.edu>
2019-11-03 18:44:49 -0500

    bobby-changed calculatir to calculate standard error for each method

--- src/main/java/analysis/CalcRunner.java
+++ src/main/java/analysis/CalcRunner.java
@@ -14,16 +14,33 @@ public class CalcRunner {
 				"./src/main/java/analysis/defaultRprec.txt"
 		};
 		
-		String[] customFilePaths = {
-				"./src/main/java/analysis/customMap.txt",
-				"./src/main/java/analysis/customNdcg.txt",
-				"./src/main/java/analysis/customRprec.txt"
+		String[] ulFilePaths = {
+				"./src/main/java/analysis/ULcustomMap.txt",
+				"./src/main/java/analysis/ULcustomNdcg.txt",
+				"./src/main/java/analysis/ULcustomRprec.txt"
 		};
+		
+		String[] udsFilePaths = {
+				"./src/main/java/analysis/UDScustomMap.txt",
+				"./src/main/java/analysis/UDScustomNdcg.txt",
+				"./src/main/java/analysis/UDScustomRprec.txt"
+		};
+		
+		String[] ujmFilePaths = {
+				"./src/main/java/analysis/UJMcustomMap.txt",
+				"./src/main/java/analysis/UJMcustomNdcg.txt",
+				"./src/main/java/analysis/UJMcustomRprec.txt"
+		};
+		
 		
 		System.out.println("Defaults file results: ");
 		runAnalysis(defaultFilePaths);
-		System.out.println("\n\nCustom file results: ");
-		runAnalysis(customFilePaths);
+		System.out.println("\n\nU-L file results: ");
+		runAnalysis(ulFilePaths);
+		System.out.println("\n\nU-JM file results: ");
+		runAnalysis(ujmFilePaths);
+		System.out.println("\n\nU-DS file results: ");
+		runAnalysis(udsFilePaths);
 		
 	}
 	
@@ -34,7 +51,7 @@ public class CalcRunner {
 			Float standDev = calc.calculateStandardDev();
 			Float standError = calc.calculateStandardError();
 			System.out.println("Results for file: " + file);
-			System.out.println("Mean: " + mean + "\tStandard Deviation: " + standDev + "\tStandard Error: " + standError + "\n");
+			System.out.println("Mean: " + mean + "\nStandard Deviation: " + standDev + "\nStandard Error: " + standError + "\n");
 		}
 	}
 }
