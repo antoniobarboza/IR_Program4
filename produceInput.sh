877b48fee3fd12b809b0d9c9c3d31969930e74f8
Tony Barboza <ajb1047@wildcats.unh.edu>
2019-11-03 18:30:11 -0500

    Tony - Updated the bash scricpt

--- produceInput.sh
+++ produceInput.sh
@@ -5,21 +5,45 @@ touch ./src/main/java/analysis/defaultRprec.txt
 touch ./src/main/java/analysis/defaultMap.txt
 touch ./src/main/java/analysis/defaultNdcg.txt
  
-./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultRprec.txt
+./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultRprec.txt
 
-./trec_eval.9.0/trec_eval -m map ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultMap.txt
+./trec_eval.9.0/trec_eval -m map ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultMap.txt
 
-./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultNdcg.txt
+./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/DefaultRankingOutput.txt -q > src/main/java/analysis/defaultNdcg.txt
 /Program2/trec_eval.9.0
-echo "Producing files for Custom Rank"
-touch ./src/main/java/analysis/customRprec.txt
-touch ./src/main/java/analysis/customMap.txt
-touch ./src/main/java/analysis/customNdcg.txt
+echo "Producing files for Custom Rank UL"
 
-./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/CustomRankingOutput.txt  -q > ./src/main/java/analysis/customRprec.txt
+touch ./src/main/java/analysis/ULcustomRprec.txt
+touch ./src/main/java/analysis/ULcustomMap.txt
+touch ./src/main/java/analysis/ULcustomNdcg.txt
 
-./trec_eval.9.0/trec_eval -m map ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/CustomRankingOutput.txt  -q > ./src/main/java/analysis/customMap.txt
+./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/ULCustomRankingOutput.txt  -q > ./src/main/java/analysis/ULcustomRprec.txt
 
-./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/data/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/CustomRankingOutput.txt  -q > ./src/main/java/analysis/customNdcg.txt
+./trec_eval.9.0/trec_eval -m map ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/ULCustomRankingOutput.txt  -q > ./src/main/java/analysis/ULcustomMap.txt
+
+./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/ULCustomRankingOutput.txt  -q > ./src/main/java/analysis/ULcustomNdcg.txt
+
+echo "Producing files for Custom Rank UJM"
+touch ./src/main/java/analysis/UJMcustomRprec.txt
+touch ./src/main/java/analysis/UJMcustomMap.txt
+touch ./src/main/java/analysis/UJMcustomNdcg.txt
+
+./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UJMCustomRankingOutput.txt  -q > ./src/main/java/analysis/UJMcustomRprec.txt
+
+./trec_eval.9.0/trec_eval -m map ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UJMCustomRankingOutput.txt  -q > ./src/main/java/analysis/UJMcustomMap.txt
+
+./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UJMCustomRankingOutput.txt  -q > ./src/main/java/analysis/UJMcustomNdcg.txt
+
+echo "Producing files for Custom Rank UDS"
+touch ./src/main/java/analysis/UDScustomRprec.txt
+touch ./src/main/java/analysis/UDScustomMap.txt
+touch ./src/main/java/analysis/UDScustomNdcg.txt
+
+./trec_eval.9.0/trec_eval -m Rprec ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UDSCustomRankingOutput.txt  -q > ./src/main/java/analysis/UDScustomRprec.txt
+
+./trec_eval.9.0/trec_eval -m map ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UDSCustomRankingOutput.txt  -q > ./src/main/java/analysis/UDScustomMap.txt
+
+./trec_eval.9.0/trec_eval -m ndcg_cut.20 ./src/main/java/test200/test200-train/train.pages.cbor-article.qrels ./src/main/java/output/UDSCustomRankingOutput.txt  -q > ./src/main/java/analysis/UDScustomNdcg.txt
+
 
 echo "DONE>>>"
